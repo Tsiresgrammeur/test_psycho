0df8a51720b0edf8da73a677f546d11e1d77654e
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2024-12-04 23:39:15 +0300

    mobile for prepare

--- resources/js/components/Prepare.vue
+++ resources/js/components/Prepare.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="xl:p-16 p-12">
+<div class="max-lg:px-28 max-lg:28 xl:p-16 p-12">
   <div class="flex items-start">
     <div class="w-full">
       <!-- Title Section -->
-      <div class="flex flex-col items-start justify-center xl:p-20 p-16 bg-white rounded-xl shadow-lg">
+      <div class="flex flex-col items-start justify-center xl:p-20 p-16 bg-white rounded-2xl shadow-xl">
         <div class="mb-8 text-center lg:text-left">
-          <h2 class="text-3xl font-title text-[#36578A]">
+          <h2 class="text-3xl font-semibold font-title text-[#36578A]">
             Préparez votre test psychotechnique
           </h2>
         </div>
@@ -13,13 +13,13 @@
           <div
             v-for="(item, index) in questions"
             :key="index"
-            class="border-b border-gray-300 py-2 text-[#585858]"
+            class="border-b border-gray-300 py-2 max-lg:pb-10 text-[#585858]"
           >
             <div
               @click="toggleSection(index)"
               class="w-full flex justify-between items-center text-left text-lg cursor-pointer"
             >
-              <div class="font-body lg:text-[1.05rem] xl:text-[1.125rem]">{{ item.question }}</div>
+              <div class="font-body max-lg:text-[28px] lg:text-[1.05rem] xl:text-[1.125rem]">{{ item.question }}</div>
               <div>
                 <i
                   :class="activeSection === index ? 'fa fa-minus' : 'fa fa-plus'"

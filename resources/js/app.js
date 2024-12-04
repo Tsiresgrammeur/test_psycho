e03f65105f1e3bd2874a42ebdd12e25951dbb8eb
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2024-12-04 15:58:12 +0300

    adding accordeon

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,10 @@ import Welcome from "./components/Welcome.vue"
 import Steps from "./components/Steps.vue"
 import Thought from "./components/Thought.vue"
 import Pink from "./components/Pink.vue"
+import Prepare from "./components/Prepare.vue"
 
 createApp(Welcome).mount('#app')
 createApp(Steps).mount('#steps')
 createApp(Thought).mount('#thought')
 createApp(Pink).mount('#pink')
+createApp(Prepare).mount('#prepare')

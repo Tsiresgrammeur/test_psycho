e5e0ef4e11eadc2b5df55d0a0fdd4977007e5397
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2024-12-06 16:51:23 +0300

    fix pixel

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-20">
+  <div class="homepage px-16 py-20">
     <div
       class="flex flex-col items-center lg:items-start justify-center text-center w-full"
     >
@@ -66,8 +66,8 @@
         </div>
       </div>
       <!-- Video Section -->
-      <div class="flex-shrink-0 lg:w-2/5 w-4/5">
-        <div class="relative pb-[56.25%] h-0">
+      <div class="flex-shrink-0 w-100">
+        <div class="hero-video relative pb-[56.25%] h-0">
           <iframe
             class="absolute top-0 left-0 w-full h-full"
             src="https://www.youtube.com/embed/zmjPmzJFEFA"
@@ -77,6 +77,7 @@
             allowfullscreen
           >
           </iframe>
+          <div class="blob-bg"></div>
         </div>
       </div>
     </div>
@@ -85,5 +86,48 @@
 <script>
 export default {};
 </script>
-<style>
+<style scoped>
+.homepage{
+  border-top: 4px solid #BF2A6B;
+  overflow: hidden;
+  margin-bottom: 2rem;
+  padding-bottom: 7rem;
+}
+.blob-bg {
+  position: absolute;
+    width: 1000px;
+    height: 642px;
+    background: #BF2A6B;
+    border-top-left-radius: 50%;
+    border-top-right-radius: 0;
+    border-bottom-left-radius: 50%;
+    border-bottom-right-radius: 50%;
+    top: 30%;
+    left: 72%;
+    z-index: -1;
+    transform: translate(-42%, -58%) rotate(-23deg);
+}
+
+.hero-video{
+  width: 500px;
+  height: 200px;
+}
+
+
+@media only screen and (max-width: 1024px)  {
+  .blob-bg {
+    position: absolute;
+    width: 411px;
+    height: 616px;
+    background: #BF2A6B;
+    border-top-left-radius: 50%;
+    border-top-right-radius: 50%;
+    border-bottom-left-radius: 50%;
+    border-bottom-right-radius: 50%;
+    top: 42%;
+    left: 69%;
+    z-index: -1;
+    transform: translate(-71%, -44%) rotate(71deg);
+  }
+}
 </style>
